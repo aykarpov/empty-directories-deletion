7b2901534996d0d64b0d0d6bbba6d8590a754095
aykarpov <aykarpov.work@gmail.com>
2016-08-13 04:22:48 +0300

    Добавлен модуль для констант

---
+++ unit_konstanty.pas
@@ -0,0 +1,25 @@
+unit unit_konstanty;
+
+
+{$mode delphi}
+
+
+interface
+
+
+
+uses
+  Classes, SysUtils;
+
+const
+  Cqto_vyvoditsaq_v_Memo = 'Список исследованных каталогов';
+
+
+
+implementation
+
+
+
+
+end.
+
